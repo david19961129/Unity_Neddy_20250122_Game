00cfedf523593ee58943328a44e64c8a66c862a9
david19961129 <david19961129@gmail.com>
2025-02-07 23:17:05 +0800

    item背包程式打到一半

---
+++ Assets/Scripts/ITEM/ITEM.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName; // 道具名称
+    public Sprite icon;     // 道具图标
+    public string description; // 道具描述
+}
