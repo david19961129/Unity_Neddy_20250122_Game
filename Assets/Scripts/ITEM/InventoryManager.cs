b84701326543cd5d1c017221a2167e7f8dd36b66
david19961129 <david19961129@gmail.com>
2025-02-11 00:17:55 +0800

    上傳背包7個口袋新數據

---
+++ Assets/Scripts/ITEM/InventoryManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NEDDY
+{
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance; // 单例模式
+
+    public List<Item> items = new List<Item>(); // 背包中的道具列表
+    public Transform itemSlotsParent; // 道具槽的父对象
+    public GameObject inventoryUI; // 背包 UI
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        // 按 Tab 键打开/关闭背包
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+
+    // 添加道具到背包
+    public void AddItem(Item newItem)
+    {
+        items.Add(newItem);
+        UpdateUI();
+    }
+
+    // 从背包移除道具
+    public void RemoveItem(Item itemToRemove)
+    {
+        items.Remove(itemToRemove);
+        UpdateUI();
+    }
+
+        // 更新背包 UI
+        private void UpdateUI()
+        {
+            // 清空所有道具槽
+            for (int i = 0; i < itemSlotsParent.childCount; i++)
+            {
+                Image slotImage = itemSlotsParent.GetChild(i).GetComponent<Image>();
+                if (slotImage != null)
+                {
+                    slotImage.sprite = null;
+                    slotImage.enabled = false;
+                }
+            }
+
+            // 更新道具槽
+            for (int i = 0; i < items.Count && i < itemSlotsParent.childCount; i++)
+            {
+                Image slotImage = itemSlotsParent.GetChild(i).GetComponent<Image>();
+                if (slotImage != null)
+                {
+                    slotImage.sprite = items[i].icon;
+                    slotImage.enabled = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ITEM/InventorySlot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace NEDDY
+{
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon; // 道具图标
+    public Button button; // 道具槽按钮
+
+    private Item item; // 当前道具
+
+    // 添加道具到槽
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+        icon.sprite = item.icon;
+        icon.enabled = true;
+        button.interactable = true;
+    }
+
+    // 清空槽
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+        button.interactable = false;
+    }
+
+    // 点击道具槽
+    public void OnSlotClick()
+    {
+        if (item != null)
+        {
+            Debug.Log("使用道具：" + item.itemName);
+            // 这里可以添加使用道具的逻辑
+        }
+    }
+}
+}
